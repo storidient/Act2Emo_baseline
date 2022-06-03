935d3596a7bf50406d45cbb9560455d5ecd4f288
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-06-03 17:23:40 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import re
 from jamo import h2j, j2hcj
 from cached_property import cached_property
+from pathlib import Path
 
 
 
@@ -15,6 +16,7 @@ def make_list(words):
   return [w for w in output if len(w) > 0]
 
 
+"""open the file"""
 def open_dir(dir):
   if dir.endswith('.xlsx'):   
     return pd.read_excel(dir, header = 0)
@@ -25,7 +27,27 @@ def open_dir(dir):
   else:
     raise Exception('The type of file should be csv or xlsx')
 
+    
+"""save_the_file"""
+def save_file(result, output_dir, file_name, output_type = xlsx):
+  output_dir = Path(output_dir)
+  output_dir.mkdir(exist_ok = True)
+  
+  file_name = re.sub('/','-',file_name)
+  
+  if output_type = 'xlsx':
+    file_name += '.xlsx'
+    pd.DataFrame(result).to_excel(output_dir / Path(file_name))
+  
+  elif output_type = 'csv':
+    file_name += '.csv'
+    pd.DataFrame(result).to_csv(output_dir / Path(file_name))
+  
+  else:
+    raise Exception('The type of file should be csv or xlsx')    
 
+    
+    
 class JongSung:
   def __init__(self, word, vowel_list = korean_vowel):
     self.word = word
