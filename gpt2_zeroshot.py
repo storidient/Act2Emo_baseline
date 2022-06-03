bff62210b322582ef9a8cf5be7535e9da250e189
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-06-03 15:47:20 +0900

    Update gpt2_zeroshot.py

--- gpt2_zeroshot.py
+++ gpt2_zeroshot.py
@@ -20,40 +20,21 @@ def find_nth(haystack, needle, n):
         n -= 1
     return start if start != -1 else None
 
-
-def main():
-  wandb.init(project="Act2Emo_keywords")
-  config = wandb.config
-
-  """Set Config"""
-  config.GPT2_MODEL = os.environ.get("GPT2_MODEL", 'skt/kogpt2-base-v2')
-  config.DATA_PATH = os.environ.get("DATA_PATH")
-  config.OUTPUT_DIR = os.environ.get("OUTPUT_DIR")
-
-  config.OUTPUT_LEN = int(os.environ.get("OUTPUT_LEN", 3))
-  config.SEED = int(os.environ.get("SEED", 42))
-
-  config.TOP_K = int(os.environ.get("TOP_K", 1))
-  config.TOP_P = int(os.environ.get("TOP_P", 0.9))
-  config.NUM_BEAMS = int(os.environ.get("NUM_BEAMS", 10))
-  config.NUM_SEQUENCES = int(os.environ.get("NUM_SEQUENCES", 10))
-  config.STOP_TOKEN = "."
-  
+def main(args):  
   """set seed"""
-  torch.manual_seed(config.SEED) # pytorch random seed
-  np.random.seed(config.SEED) # numpy random seed
+  torch.manual_seed(args.SEED) # pytorch random seed
+  np.random.seed(args.SEED) # numpy random seed
   torch.backends.cudnn.deterministic = True
 
   """Set Model"""
-  model_name = config.GPT2_MODEL
+  model_name = args.GPT2_MODEL
 
-  tokenizer = AutoTokenizer.from_pretrained(config.GPT2_MODEL)
-  model = AutoModelForCausalLM.from_pretrained(config.GPT2_MODEL)
+  tokenizer = AutoTokenizer.from_pretrained(args.GPT2_MODEL)
+  model = AutoModelForCausalLM.from_pretrained(args.GPT2_MODEL)
   model.to(device)
-  wandb.watch(model, log="all")
 
   """Download Data"""
-  BDdataset = BodyDataset(config.DATA_PATH)
+  BDdataset = BodyDataset(args.DATA_PATH)
   
   result = list()
   for idx, row in tqdm.tqdm(enumerate(BDdataset)):
@@ -63,14 +44,14 @@ def main():
 
     generations = model.generate(
         input_ids=input_ids.to(device),
-        max_length=input_ids.size(1) + config.SUMMARY_LEN,
+        max_length=input_ids.size(1) + args.OUTPUT_LEN,
         temperature=1.0,
-        top_k=config.TOP_K,
-        top_p=config.TOP_P,
+        top_k=args.TOP_K,
+        top_p=args.TOP_P,
         repetition_penalty=1.0,
         do_sample=True,
-        num_return_sequences=config.NUM_SEQUENCES,
-        num_beams=config.NUM_BEAMS
+        num_return_sequences=args.NUM_SEQUENCES,
+        num_beams=args.NUM_BEAMS
     )
 
     if len(generations.shape) > 2:
@@ -81,7 +62,7 @@ def main():
     for gen in generations:
         gen = gen.tolist()
         text = tokenizer.decode(gen, clean_up_tokenization_spaces=True)
-        text = text[:find_nth(text, config.STOP_TOKEN, 1)] if config.STOP_TOKEN not in input else text[:find_nth(text, config.STOP_TOKEN, 2)]
+        text = text[:find_nth(text, args.STOP_TOKEN, 1)] if config.STOP_TOKEN not in input else text[:find_nth(text, args.STOP_TOKEN, 2)]
         text_generations.append(text)
 
     result.append({
@@ -93,17 +74,36 @@ def main():
         "generations": text_generations
         })
     
-  output_path = Path(config.OUTPUT_DIR)
+  output_path = Path(args.OUTPUT_DIR)
   output_path.mkdir(exist_ok = True)
 
-  f_name = '{}-{}-{}-{}-{}.xlsx'.format(re.sub('/','-', config.GPT2_MODEL), 
-                                  config.TOP_K,
-                                  config.TOP_P,
-                                  config.NUM_BEAMS,
-                                  config.SUMMARY_LEN)
+  f_name = '{}-{}-{}-{}-{}.xlsx'.format(re.sub('/','-', args.GPT2_MODEL), 
+                                        args.TOP_K,
+                                        args.TOP_P,
+                                        args.NUM_BEAMS,
+                                        args.OUTPUT_LEN)
   
   pd.DataFrame(list_dict).to_excel(output_path / Path(f_name))
 
 
 if __name__ == "__main__":
-    main()
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument("--GPT2_MODEL", type=str, default='skt/kogpt2-base-v2',
+                        choices=['skt/kogpt2-base-v2', "conceptnet"])
+    
+    parser.add_argument("--DATA_PATH", type=str)
+    parser.add_argument("--OUTPUT_DIR", type=str, default = './output/')
+    parser.add_argument("--OUTPUT_LEN", type=int, default = 3)
+    parser.add_argument("--SEED", type=int, default = 42)
+    parser.add_argument("--STOP_TOKEN", type=str, default = ".")
+    
+    parser.add_argument("--TOP_K", type=int, default = 1)
+    parser.add_argument("--TOP_P", type=int, default = 0.9)
+    parser.add_argument("--NUM_BEAMS", type=int, default = 10)
+    parser.add_argument("--NUM_SEQUENCES", type=int, default = 10)
+
+    args = parser.parse_args()
+    
+    main(args)
