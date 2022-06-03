a9f8419069b10ec5f60ef5d2d58704dc9ecd32ec
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-06-03 16:32:35 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,10 +4,14 @@ from jamo import h2j, j2hcj
 korean_vowel = ['ㅏ','ㅑ', 'ㅓ', 'ㅕ', 'ㅗ', 'ㅛ', 'ㅜ', 'ㅠ', 'ㅡ','ㅣ','ㅒ', 
               'ㅐ','ㅔ', 'ㅖ', 'ㅟ', 'ㅚ', 'ㅙ', 'ㅞ']
 
+"""a string to a list"""
+def make_list(words):
+  output = [re.sub('[^가-힣]', '', w) for w in words.split(',')]
+  return [w for w in output if len(w) > 0]
+
 """extract jongsung from a word"""
 def extract_jongsung(word):
   return j2hcj(h2j(word[-1]))[-1]
-
 
 """decide jongsung type for josa"""
 def decide_jongsung(word, vowel_list=korean_vowel):
