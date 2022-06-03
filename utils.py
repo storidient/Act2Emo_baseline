dc7a444fe48e4ad75b8daca9d842ba1f305a300f
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-06-03 16:41:00 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,28 +1,33 @@
+import re
 from jamo import h2j, j2hcj
+from cached_property import cached_property
+
+
 
 """a list of vowels"""
 korean_vowel = ['ㅏ','ㅑ', 'ㅓ', 'ㅕ', 'ㅗ', 'ㅛ', 'ㅜ', 'ㅠ', 'ㅡ','ㅣ','ㅒ', 
               'ㅐ','ㅔ', 'ㅖ', 'ㅟ', 'ㅚ', 'ㅙ', 'ㅞ']
+
 
 """a string to a list"""
 def make_list(words):
   output = [re.sub('[^가-힣]', '', w) for w in words.split(',')]
   return [w for w in output if len(w) > 0]
 
-"""extract jongsung from a word"""
-def extract_jongsung(word):
-  return j2hcj(h2j(word[-1]))[-1]
 
-"""decide jongsung type for josa"""
-def decide_jongsung(word, vowel_list=korean_vowel):
-  jongsung = extract_jongsung(word)
+class JongSung:
+  def __init__(self, word, vowel_list = korean_vowel):
+    self.word = word
+    self.vowel, self.liquid = False, False
+    self._build(vowel_list)
+      
+  @cached_property
+  def jongsung(self):
+    return j2hcj(h2j(self.word[-1]))[-1]
   
-  vowel, liquid = False, False
-  
-  if jongsung in vowel_list:
-    vowel, liquid = True, True
-
-  if jongsung == 'ㄹ':
-    liquid = True
-          
-  return vowel, liquid
+  def _build(self, vowel_list):
+    if self.jongsung in vowel_list:
+      self.vowel, self.liquid = True, True
+      
+    elif self.jongsung in vowel_list:
+      self.liquid = True
