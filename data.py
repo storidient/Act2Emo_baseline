8a9147637060a14f09309652c708956671756d4c
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-06-06 10:22:18 +0900

    Update data.py

--- data.py
+++ data.py
@@ -14,27 +14,32 @@ josa_dict['vowel'] = [
                       ('는', '은')
                       ]
 
-
-
 """inherit class Jongsung and add prompts"""
 class AddPrompts(Jongsung):
-  def __init__(self, word, josa = josa_dict):
+  def __init__(self, word, model, length, josa = josa_dict):
     super().__init__(word)
     
-    self.prompts = self.make_prompts(josa)
+    self.length = length
+    self.prompts = self.make_prompts(josa, model)
     self.input = self.add_prompts()
   
   """make a list of prompts"""
-  def make_prompts(self, josa):
+  def make_prompts(self, josa, model):
     prompts = josa['common']
     prompts += [t[0] if self.vowel == True else t[1] for t in josa['vowel']]
     prompts += ['으' + t if self.liquid == False else t for t in sorted(josa['liquid'])]
-
+    
+    if model in ['bert', 'roberta']:
+      prompts = sum(list(map(self.add_mask, prompts)), [])
+    
     return prompts
   
+  def add_mask(self, prompt):
+      return [prompt + ' ' + '<mask>' * i + '다.' for i in range(self.length+1)]
+    
   """make a list of words"""
   def add_prompts(self):
-    return list(map(lambda p : self.word + p, self.prompts))
+      return list(map(lambda p : self.word + p, self.prompts))
   
   def __getitem__(self, idx):
     return (self.prompts[idx], self.input[idx])
@@ -42,9 +47,11 @@ class AddPrompts(Jongsung):
   
   
 class BodyDataset:
-  def __init__(self, dir):
+  def __init__(self, dir, model = 'gpt', length = 0):
     self.data = open_dir(dir)
-   
+    self.model = model
+    self.length = length
+    
     self.keywords = self.data['Keywords']
     self.cat = self.data['Category']
     self.subcat = self.data['SubCategory']
@@ -56,7 +63,8 @@ class BodyDataset:
     for idx, string in enumerate(self.keywords):
       for word in make_list(string):
         item_list += [(input, prompt, word, self.cat[idx], self.subcat[idx]) 
-                      for prompt, input in AddPrompts(word)]
+                      for prompt, input in AddPrompts(word, model = self.model, length = self.length)]
+    
     return item_list
          
   def __len__(self):
