a305babbfc9ee5bc51955eee8b289a260ff2b9dc
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-06-22 13:16:00 +0900

    Update preprocessing.py

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -4,7 +4,6 @@ from cached_property import cached_property
 from boltons.iterutils import pairwise
 
 
-
 class Download:
   def __init__(self, dir):
     self.dir = dir
@@ -17,3 +16,24 @@ class Download:
 
   def clean_txt(self, line):
     return line.replace('\n', '').replace(u'\xa0', u' ').strip()
+
+  
+class RxLogging:
+  def __init__(self, logger):
+    self.logger = logger
+    self.show_key = list()
+
+  def show(self, key):
+    self.show_key = key if type(key) == list else [key]
+  
+  def print(self, key, message):
+    self.logger.info(message) if key in self.show else self.logger.debug(message)
+
+  def check(self, keys, pattern):
+    keys = keys if type(keys) == list else [keys]
+    undefined = [key for key in keys if key not in pattern]
+
+    if len(undefined):
+      self.logger.warning('Undefined key : %s' % ('/'.join(undefined)))
+
+    return list(set(keys)- set(undefined))
